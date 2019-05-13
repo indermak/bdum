4cfcf5d9281c37cdeba4d4aec4d5d210376819a4
indermak <i.mak1992@gmail.com>
2019-05-13 16:07:34 +0530

    added branch locator

--- js/custom.js
+++ js/custom.js
@@ -112,3 +112,17 @@ $('.owl-three').owlCarousel({
     }
 })
 });
+
+
+//Counter
+$('.count').each(function() {
+  $(this).prop('Counter', 0).animate({
+    Counter: $(this).text()
+  }, {
+    duration: 8000,
+    easing: 'swing',
+    step: function(now) {
+      $(this).text(Math.ceil(now));
+    }
+  });
+});
